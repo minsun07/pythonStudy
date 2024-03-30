56543dfcbc5106a4c42256eb37077f8ea1ce1f7f
An minsun <elleto123456@gmail.com>
2024-03-30 16:31:34 +0900

    정수형

---
+++ integer.py
@@ -0,0 +1,23 @@
+#정수형
+print(8+5)
+print(8-5)
+print(8*5)
+print(8/5) #계산한 전체 값
+print(8//5) #몫
+print(8%5) #나머지
+print(2**6) #거듭제곱
+
+age = 18 #자동으로 타입이 정해짐
+print(age)  #18
+after_2 = 2
+print(age + after_2) #18+2=20
+result = age - after_2 #18-2=16
+print(result) #16
+
+age = 18
+age += 2 #20
+print(age)
+age -= 2 #18
+print(age)
+age *= 2 # 36
+print(age)
