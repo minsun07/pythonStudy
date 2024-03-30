2c05c8f70767c3e6593942601737e5f6f104195a
An minsun <elleto123456@gmail.com>
2024-03-30 23:05:01 +0900

    리스트 기본

---
+++ List.py
@@ -0,0 +1,9 @@
+#리스트[]
+l = []
+print(l)
+player = ["Messi", 10, True]  #['Messi', 10, True]
+print(player)
+print(list("Happy")) #['H', 'a', 'p', 'p', 'y']
+print({"menu":"pizza", "price":10000})  #{'menu': 'pizza', 'price': 10000}
+nums = list(range(3)) #[0, 1, 2]
+print(nums)
