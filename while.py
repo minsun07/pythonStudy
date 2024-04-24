b2d7ced83224e89c245c61b77ccf5caf14529081
An minsun <elleto123456@gmail.com>
2024-04-24 22:15:21 +0900

    while문

---
+++ while.py
@@ -0,0 +1,20 @@
+x = 3
+# while x < 6: print(x)   # 3을 무한반복
+while x < 6:
+   print(x)   # 3  4  5
+   x += 1
+
+
+echo = input()
+while echo != "exit":
+   print(echo)
+   echo = input() #새로 입력을 받아 변수를 바꾼다.
+
+
+#print("-----------------------------------")
+#echo = input()
+#while True:  #코드 블록을 무한 반복한다.
+#    if echo == "exit":
+#        break  #조건문을 만족하면 반복문을 멈춘다.
+#    print(echo)
+#    echo = input() #새로 입력을 받아 변수를 바꾼다.
